055ef9c67e7134106d16637f1132b397e4abf2af
ThePope05 <swobben05@gmail.com>
2023-05-10 11:10:22 +0200

    Initial commit
    Added index and basic class

---
+++ js/classes/Person.js
@@ -0,0 +1,15 @@
+class Person{
+    Name;
+    Health;
+    Stamina;
+    Armor;
+    Level;
+
+    constructor(name, health, stamina, armor, level){
+        this.Name = name;
+        this.Health = health;
+        this.Stamina = stamina;
+        this.Armor = armor;
+        this.Level = level;
+    }
+}
